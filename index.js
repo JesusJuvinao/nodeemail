8593561a423d38dd37e4b58bdc9aa96eab2f8904
jesusjuvinao <jesus.juvinao@smartaccountingonline.com>
2021-12-10 16:24:09 -0500

    config nodemon

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.send('Hola Stuart'))
+app.get('/', (req, res) => {
+    res.send('Hola Stuart')
+})
 app.listen(3000)
 console.log('Estas en el puerto 3000')
