cd705f62f38984f56e6680d40260941d5e599b7b
JesusJuvinao <jesus.juvinao@smartaccountingonline.com>
2021-12-20 19:57:00 +0100

    push config babel

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 /* eslint-disable no-param-reassign */
 import express from 'express'
-import puppeteer from 'puppeteer'
 import cron from 'node-cron'
+const fs = require('fs')
 cron.schedule('15 * * * *', () => {
     console.log('hola hola hola')
 })
 const app = express()
-// const iPhone = puppeteer.devices['iPhone 6']
-const fs = require('fs')
 app.get('/', (req, res) => {
     res.send('Server online')
 })
